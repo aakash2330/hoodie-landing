2ce0def88d1239f219321d901d6e6c653d5e2455
aakash2330 <aakash2330@gmail.com>
2024-05-24 12:06:21 +0530

    hoodiepage

--- src/app/hoodie/[id]/page.tsx
+++ src/app/hoodie/[id]/page.tsx
@@ -1,3 +1,119 @@
-export default function Page(){
-  return <div>Hoodie</div>
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { ArrowLeft, ArrowRight } from "lucide-react";
+import { useEffect } from "react";
+
+export default function Page() {
+  useEffect(() => {
+    const slider = document.querySelector(".slider");
+
+    function activate(e: any) {
+      const items = document.querySelectorAll(".item");
+      e.target.matches(".next") && slider?.append(items[0]);
+      e.target.matches(".prev") && slider?.prepend(items[items.length - 1]);
+    }
+
+    document.addEventListener("click", activate, false);
+  }, []);
+  return (
+    <main>
+      <ul className="slider">
+        <li
+          className="item"
+          style={{
+            backgroundImage:
+              "url('https://aakash2330-drippy.s3.amazonaws.com/hoodie/kinsley+Review.avif')",
+          }}
+        >
+          <div className="content">
+            <h2 className="title">"Why us 1"</h2>
+            <p className="description">Lengthy Product Description . Lengthy Product Description . Lengthy Product Description . Lengthy Product Description</p>
+            <button>Place Order</button>
+          </div>
+        </li>
+        <li
+          className="item"
+          style={{
+            backgroundImage:
+              "url('https://aakash2330-drippy.s3.amazonaws.com/hoodie/final+output.jpg')",
+          }}
+        >
+          <div className="content">
+            <h2 className="title">"Why us 1"</h2>
+
+            <p className="description">Lengthy Product Description . Lengthy Product Description . Lengthy Product Description . Lengthy Product Description</p>
+
+            <button>Place Order</button>
+          </div>
+        </li>
+        <li
+          className="item"
+          style={{
+            backgroundImage:
+              "url('https://aakash2330-drippy.s3.amazonaws.com/hoodie/DSC00720.JPG')",
+          }}
+        >
+          <div className="content">
+            <h2 className="title">"Why us 1"</h2>
+
+            <p className="description">Lengthy Product Description . Lengthy Product Description . Lengthy Product Description . Lengthy Product Description</p>
+
+            <button>Place Order</button>
+          </div>
+        </li>
+        <li
+          className="item"
+          style={{
+            backgroundImage:
+              "url('https://aakash2330-drippy.s3.amazonaws.com/hoodie/Glovess.webp')",
+          }}
+        >
+          <div className="content">
+            <h2 className="title">"Why us 1"</h2>
+
+            <p className="description">Lengthy Product Description . Lengthy Product Description . Lengthy Product Description . Lengthy Product Description</p>
+
+            <button>Place Order</button>
+          </div>
+        </li>
+        <li
+          className="item"
+          style={{
+            backgroundImage:
+              "url('https://aakash2330-drippy.s3.amazonaws.com/hoodie/final+output.jpg')",
+          }}
+        >
+          <div className="content">
+            <h2 className="title">"Urban Decay"</h2>
+
+            <p className="description">Lengthy Product Description . Lengthy Product Description . Lengthy Product Description . Lengthy Product Description</p>
+
+            <button>Place Order</button>
+          </div>
+        </li>
+        <li
+          className="item"
+          style={{
+            backgroundImage:
+              "url('https://aakash2330-drippy.s3.amazonaws.com/hoodie/gloves3.jpg')",
+          }}
+        >
+          <div className="content">
+            <h2 className="title">"Why us 1"</h2>
+            <p className="description">Lengthy Product Description . Lengthy Product Description . Lengthy Product Description . Lengthy Product Description</p>
+            <button>Place Order</button>
+          </div>
+        </li>
+      </ul>
+      <nav className="nav flex gap-3">
+        <Button className=" rounded-2xl border prev  flex items-center justify-center">
+          Prev
+        </Button>
+        <Button className=" rounded-2xl border next  flex items-center justify-center">
+          Next
+        </Button>
+      </nav>
+    </main>
+  );
 }
